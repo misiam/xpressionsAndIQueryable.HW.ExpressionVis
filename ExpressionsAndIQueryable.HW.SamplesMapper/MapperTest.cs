a7dc6adb912c37795f1443bfd0a9f7f9a14e49a7
Mikhail_Marykin <Mikhail_Marykin@epam.com>
2017-04-03 15:08:09 +0300

    [Expression Trees] Class mapper

---
+++ ExpressionsAndIQueryable.HW.SamplesMapper/MapperTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionsAndIQueryable.HW.Mapper
+{
+    public class Foo {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FullName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id} Name:{Name} FullName:{FullName}";
+        }
+    }
+
+    public class Bar
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id:{Id} Name:{Name} LastName:{LastName}";
+        }
+    }
+
+
+
+    [TestClass]
+    public class MapperTest
+    {
+        [TestMethod]
+        public void TestMappingFooToBar()
+        {
+            var mapGenerator = new MappingGenerator();
+            var mapper = mapGenerator.Generate<Foo, Bar>();
+
+            Foo foo = new Foo() {Id = 42, Name = "From foo", FullName = "Full foo name"};
+            Bar bar = mapper.Map(foo);
+
+            Console.WriteLine("foo: {0}",foo);
+            Console.WriteLine("bar: {0}", bar);
+        }
+    }
+
+
+}
---
+++ ExpressionsAndIQueryable.HW.SamplesMapper/MappingGenerator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ExpressionsAndIQueryable.HW.Mapper
+{
+    public class MappingGenerator
+    {
+        public Mapper<TSource, TDestination> Generate<TSource, TDestination>() where TDestination : new()
+        {
+            var sourceParam = Expression.Parameter(typeof (TSource));
+            var destType = typeof (TDestination);
+            var ctor = Expression.New(destType);
+            var initializedCtor = Expression.MemberInit(ctor, CreateMemberBindings(destType, sourceParam));
+
+            var mapFunction =
+                Expression.Lambda<Func<TSource, TDestination>>(initializedCtor, sourceParam);
+
+            return new Mapper<TSource, TDestination>(mapFunction.Compile());
+        }
+
+
+        private IList<MemberBinding> CreateMemberBindings(Type ctorType, ParameterExpression param)
+        {
+            var sourceType = param.Type;
+
+            var sourceProperties = sourceType.GetProperties().Where(x => x.CanRead).ToList();
+            var destinationProperties = ctorType.GetProperties().Where(x => x.CanWrite).ToList();
+
+            IList<MemberBinding> memberBindings = new List<MemberBinding>();
+
+            foreach (var sourceProp in sourceProperties)
+            {
+                var propName = sourceProp.Name;
+                var destProp = destinationProperties.FirstOrDefault(x => x.Name == propName);
+                if (destProp == null)
+                {
+                    continue;
+                }
+
+                var accessMember = Expression.MakeMemberAccess(param, sourceProp);
+                var assignMember = Expression.Bind(destProp, accessMember);
+                memberBindings.Add(assignMember);
+            }
+
+            return memberBindings;
+        }
+    }
+}
